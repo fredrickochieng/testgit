46e9f397511cdfe0eb5ad9592b83568dac21138f
fredrickochieng <fredrickochieng2013@gmail.com>
2017-01-15 21:03:33 +0300

    Add files via upload

---
+++ hotelcost1.py
@@ -0,0 +1,16 @@
+def hotel_cost(nights):
+    Accomodation=140*nights
+    return  Accomodation
+    print "Your Accomodation in %s will be :" +  Accomodation %s (city)
+
+def plane_ride_cost(city):
+    if city=="Charlotte":
+       return 183
+    elif city=="Tampa":
+       return 220
+    elif city=="Pittsburgh":
+       return 222
+    elif city=="Los Angeles":
+       return 475
+print hotel_cost(80)
+print plane_ride_cost("Tampa")
